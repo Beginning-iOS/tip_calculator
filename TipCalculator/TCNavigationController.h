3b53d68bf71d236964148d7261fb1abcd87cea48
Mark Haskamp <mark.haskamp@pinnsg.com>
2013-11-04 21:11:36 -0500

    added navigation controller

---
+++ TipCalculator/TCNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  TCNavigationController.h
+//  TipCalculator
+//
+//  Created by Mark Haskamp on 11/4/13.
+//  Copyright (c) 2013 Mark Haskamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCNavigationController : UINavigationController
+
+@end
---
+++ TipCalculator/TCNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  TCNavigationController.m
+//  TipCalculator
+//
+//  Created by Mark Haskamp on 11/4/13.
+//  Copyright (c) 2013 Mark Haskamp. All rights reserved.
+//
+
+#import "TCNavigationController.h"
+
+@interface TCNavigationController ()
+
+@end
+
+@implementation TCNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
